3908b6087e38b8420eb5c9dc1b8c90c53027b95e
JavBautista <jav.bautista@gmail.com>
2025-05-19 15:19:19 -0600

    a3

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    //'allowed_origins' => ['*'],
-    'allowed_origins' => [
-        'capacitor://localhost',
-        'http://localhost',
-        'http://localhost:8100',
-        'https://j2b.levcore.app',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
